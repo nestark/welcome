d8d933d468bc9b081554cbc3fc81765cb86c7197
nestark <45419176+nestark@users.noreply.github.com>
2019-08-15 11:50:48 +0800

    1 delete invalid comments 2 forbid close window

--- screen3.py
+++ screen3.py
@@ -1,7 +1,8 @@
-import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
+
 from PIL import ImageTk, Image
-from os import makedirs
+from os import makedirs, path
 
 class CanvasButton:
     def __init__(self, canvas):
@@ -16,18 +17,15 @@ class CanvasButton:
         e.set('请输入用户名')
         #self.valid = Label(canvas, bg='')
         self.name = Label(canvas,text = '用户名:').place(x=w/2-100,y=h/2-100)
-        #self.name.grid(row=0, column=5)
         self.nameInput = Entry(canvas,textvariable=e)
         self.nameInput.place(x=w/2-50,y=h/2-100)
-        #self.nameInput.grid(row=0, column=6,sticky='E',ipadx=0)
         self.password = Label(canvas, text = '密码:').place(x=w/2-100,y=h/2-80)
-        #self.password.grid(row=1, column=5)
         self.pwInput = Entry(canvas,show='*')
         self.pwInput.place(x=w/2-50,y=h/2-80)
-        #self.pwInput.grid(row=1, column=6,sticky='E',ipadx=0)
+
 
     def buttonclicked(self):
-        #self.number.set(self.number.get()+1) # auto updates Button
+        #self.number.set('Waiting')
         names = self.nameInput.get()
         passwords = self.pwInput.get()
         with open(namepath,'w') as file_object:
@@ -36,24 +34,29 @@ class CanvasButton:
             file_object.write(passwords)
         exit()
 
+if __name__ == "__main__":
 
-root = Tk()
-root.resizable(width=False, height=False)
-root.wm_attributes("-topmost", 1)
-root.attributes("-fullscreen", 1)
-makedirs('c:\\ittools')
-namepath = r'c:\ittools\aa.txt'
-pwpath = r'c:\ittools\pp.txt'
-w = root.winfo_screenwidth()
-h = root.winfo_screenheight()
-if w ==1920:
-    img = PhotoImage(file='1.png')
-elif w == 1366:
-    img = PhotoImage(file='2.png')
+    def callback():
+        messagebox.showwarning('请勿关闭窗口')
+    root = Tk()
+    root.resizable(width=False, height=False)
+    root.wm_attributes("-topmost", 1)
+    root.attributes("-fullscreen", 1)
 
-canvas = Canvas(root, bd=0, height = h, width = w, highlightthickness=0)
-canvas.pack(fill=BOTH, expand=YES )
-canvas.create_image(w/2+1, h/2+1, image=img)
+    if not path.exists('c:\\ittools') :
+        makedirs('c:\\ittools')
+    namepath = r'c:\ittools\aa.txt'
+    pwpath = r'c:\ittools\pp.txt'
+    w = root.winfo_screenwidth()
+    h = root.winfo_screenheight()
+    if w ==1920:
+        img = PhotoImage(file='1.png')
+    elif w == 1366:
+        img = PhotoImage(file='2.png')
 
-CanvasButton(canvas) # create a clickable button on the canvas
-root.mainloop()
+    canvas = Canvas(root, bd=0, height = h, width = w, highlightthickness=0)
+    canvas.pack(fill=BOTH, expand=YES )
+    canvas.create_image(w/2+1, h/2+1, image=img)
+    CanvasButton(canvas) # create a clickable button on the canvas
+    root.protocol("WM_DELETE_WINDOW", callback)
+    root.mainloop()
