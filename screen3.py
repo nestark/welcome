3101f652425c6f15f9c150898d2cb4b2d9aa9056
nestark <nestark@users.noreply.github.com>
2019-08-16 11:52:45 +0800

    1 do not create path 2 remove tips text in entry box 3 remove label next to entry box 4 add new page function(not use) 5 Tell if the entry box is empty then show warning 6 add FullScreenApp class: fullscreen and top most the window,can't be terminated by alt+F4

--- screen3.py
+++ screen3.py
@@ -1,62 +1,92 @@
 from tkinter import *
 from tkinter import messagebox
-
-from PIL import ImageTk, Image
-from os import makedirs, path
+'''from os import makedirs, path'''
 
 class CanvasButton:
     def __init__(self, canvas):
         self.canvas = canvas
-        self.number = IntVar()
-        self.button = Button(canvas, text = "OK", command=self.buttonclicked)
-        self.id = canvas.create_window(w/2+1, h/2+1, width=50, height=25, window=self.button)
+        '''self.number = IntVar()'''
+        self.button = Button(canvas, image=img1, bd = 0,command=self.buttonclicked)
+        self.id = canvas.create_window(w/2, h/2+17,  window=self.button)  # create button and put it on canvas window
         self.createWidgets(canvas)
 
     def createWidgets(self, canvas):
-        e= StringVar()
+        '''e= StringVar()
         e.set('请输入用户名')
-        #self.valid = Label(canvas, bg='')
-        self.name = Label(canvas,text = '用户名:').place(x=w/2-100,y=h/2-100)
-        self.nameInput = Entry(canvas,textvariable=e)
-        self.nameInput.place(x=w/2-50,y=h/2-100)
-        self.password = Label(canvas, text = '密码:').place(x=w/2-100,y=h/2-80)
-        self.pwInput = Entry(canvas,show='*')
-        self.pwInput.place(x=w/2-50,y=h/2-80)
+        self.name = Label(canvas,text = '用户名:').place(x=w/2-100,y=h/2-100)'''  # make a label next to entry box
+        self.nameInput = Entry(canvas)
+        self.nameInput.place(x=w/2-115,y=h/2-103,width = 230)  # create Entry box
+        '''self.password = Label(canvas, text = '密码:').place(x=w/2-100,y=h/2-80)'''
+        self.pwInput = Entry(canvas,show='*')  # show password as *
+        self.pwInput.place(x=w/2-115,y=h/2-38,width = 230)
 
+    '''def newpage(self):  # exit current window and open a new window
+        root.destroy()
+        global root1
+        root1=Tk()
+        img2 = PhotoImage(file='cmd1.png')
+        label_img = Label(root1, image=img2)
+        label_img.pack()
+        app = FullScreenApp(root1)
+        app.toggle_fullscreen()
+        root1.mainloop()'''
 
     def buttonclicked(self):
         #self.number.set('Waiting')
         names = self.nameInput.get()
         passwords = self.pwInput.get()
-        with open(namepath,'w') as file_object:
-            file_object.write(names)
-        with open(pwpath,'w') as file_object:
-            file_object.write(passwords)
-        exit()
+        if names == '' or passwords =='':
+            messagebox.showwarning(title = 'Error',message ='用户名密码不能为空 DomainUser or password must be filled')
+        else:
+            with open(namepath,'w') as file_object:  # write username to namepath
+                file_object.write(names)
+            with open(pwpath,'w') as file_object:  # write password to pwpath
+                file_object.write(passwords)
+            '''self.newpage()'''
+            root.destroy()  # end the window
+
+
+
+class FullScreenApp(object):
+    def __init__(self, master):
+        self.root = master
+        self.frame = Frame(self.root)
+        self.frame.pack()
+        self.state = True
+        self.root.protocol("WM_DELETE_WINDOW", self.callback)
+    def toggle_fullscreen(self, event=None):
+        #self.state = not self.state  # Just toggling the boolean
+        self.root.attributes("-fullscreen", self.state)
+        self.root.wm_attributes("-topmost", 1)
+        return "break"
+
+    def callback(self):
+        messagebox.showwarning('请勿关闭窗口')
 
 if __name__ == "__main__":
 
-    def callback():
-        messagebox.showwarning('请勿关闭窗口')
-    root = Tk()
-    root.resizable(width=False, height=False)
-    root.wm_attributes("-topmost", 1)
-    root.attributes("-fullscreen", 1)
 
-    if not path.exists('c:\\ittools') :
-        makedirs('c:\\ittools')
+    root = Tk()
+    '''root.resizable(width=False, height=False)
+    root.wm_attributes("-topmost", 1)
+    root.attributes("-fullscreen", 1)'''
+    app = FullScreenApp(root)
+    app.toggle_fullscreen()
+    '''if not path.exists('c:\\ittools'):
+        makedirs('c:\\ittools')'''  # make dir when path not exist
     namepath = r'c:\ittools\aa.txt'
     pwpath = r'c:\ittools\pp.txt'
     w = root.winfo_screenwidth()
     h = root.winfo_screenheight()
-    if w ==1920:
+    img = PhotoImage(file = 'cmd.png')
+    img1 = PhotoImage(file = 'Enter.png')
+    '''if w ==1920:
         img = PhotoImage(file='1.png')
     elif w == 1366:
-        img = PhotoImage(file='2.png')
+        img = PhotoImage(file='2.png')'''
 
-    canvas = Canvas(root, bd=0, height = h, width = w, highlightthickness=0)
+    canvas = Canvas(root, bd=0, height = h, width = w, highlightthickness=0,bg = 'black')
     canvas.pack(fill=BOTH, expand=YES )
-    canvas.create_image(w/2+1, h/2+1, image=img)
+    canvas.create_image(w/2+1, h/2, image=img)
     CanvasButton(canvas) # create a clickable button on the canvas
-    root.protocol("WM_DELETE_WINDOW", callback)
     root.mainloop()
---
+++ screen4.py
@@ -0,0 +1,36 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
+class FullScreenApp(object):
+    def __init__(self, master):
+        self.root = master
+        self.frame = Frame(self.root)
+        self.frame.pack()
+        self.root.protocol("WM_DELETE_WINDOW", self.callback)
+    def toggle_fullscreen(self):
+        self.root.attributes("-fullscreen", True)
+        self.root.wm_attributes("-topmost", 1)
+        return "break"
+
+    def callback(self):
+        messagebox.showwarning('请勿关闭窗口')
+
+if __name__ == "__main__":
+    root1 = Tk()
+    def resize_image(event):
+        new_width = event.width
+        new_height = event.height
+        image = copy_of_image.resize((new_width, new_height))
+        photo = ImageTk.PhotoImage(image)
+        label_img.config(image=photo)
+        label_img.image = photo  # avoid garbage collection
+    img2 = Image.open('cmd1.png')
+    copy_of_image = img2.copy()
+    photo = ImageTk.PhotoImage(img2)
+    label_img = Label(root1, image=photo)
+    label_img.bind('<Configure>',resize_image)
+    label_img.pack(fill=BOTH, expand = YES)
+    app = FullScreenApp(root1)
+    app.toggle_fullscreen()
+    root1.mainloop()
