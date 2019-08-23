309dad0c233bcc7fedb419f18f098bf1275d8256
nestark <nestark@users.noreply.github.com>
2019-08-23 13:16:47 +0800

    create checkboxes for locations select

--- screen5.py
+++ screen5.py
@@ -1,7 +1,5 @@
 from tkinter import *
-from tkinter import messagebox
 from screen3 import FullScreenApp
-
 class CanvasButton:
     def __init__(self, canvas):
         self.canvas = canvas
@@ -9,25 +7,30 @@ class CanvasButton:
         self.createWidgets(canvas)
 
     def createWidgets(self, canvas):
-        '''e= StringVar()
-        e.set('请输入用户名')
-        self.name = Label(canvas,text = '用户名:').place(x=w/2-100,y=h/2-100)'''  # make a label next to entry box
-        check1 = Radiobutton(canvas, text='成都')
-        check2 = Radiobutton(canvas, text='上海')
-        check3 = Radiobutton(canvas, text='深圳')
-        check4 = Radiobutton(canvas, text='大连')
+        locations=('达美','雍和','成都','上海','大连','深圳')
+        self.shorts=('dm','yh','cd','sh','dl','sz')
+        self.intVar = IntVar()
+        i = 1
+        for location in locations:
+            if i<=3:
+                Radiobutton(canvas, text=location,variable = self.intVar,# 将Radiobutton绑定到self.intVar变量
+                    value=i,font = ('KaiTi',13),bg='#f8622f',fg = 'white',highlightcolor='black',selectcolor='#fd916c',
+                            indicatoron=0).place(x=w/2-160+65*i,y=h/2-95)
+            else:
+                Radiobutton(canvas, text=location, variable=self.intVar,
+                            value=i,font = ('KaiTi',13),bg='#f8622f',fg = 'white',highlightcolor='black',
+                            selectcolor='#fd916c',indicatoron=0).place(x=w/2-160+65*(i-3), y=h / 2 - 65)
+            i+=1
+        self.intVar.set(1)
         button = Button(canvas, image=img1, bd=0, command=self.buttonclicked)
         self.id = canvas.create_window(w / 2, h / 2 -15, window=button)  # create button and put it on canvas window
 
     def buttonclicked(self,*args):
-        #self.number.set('Waiting')
-        names = self.nameInput.get()
-        if names == '':
-            messagebox.showwarning(title = 'Error',message ='员工号不能为空 Employee ID must be filled in')
-        else:
-            with open(Epath,'w') as file_object:  # write username to namepath
-                file_object.write(names)
-            root.destroy()  # end the window
+        num = self.intVar.get()
+        names = self.shorts[num-1]
+        with open(Epath,'w') as file_object:  # write username to namepath
+            file_object.write(names)
+        root.destroy()  # end the window
 
 if __name__ == "__main__":
     root = Tk()
@@ -35,6 +38,7 @@ if __name__ == "__main__":
     app.toggle_fullscreen()
     w = root.winfo_screenwidth()
     h = root.winfo_screenheight()
+    Epath=r'c:\ittools\zz.txt'
     img = PhotoImage(file='zhichang.png')
     img1 = PhotoImage(file='Enter.png')
     canvas = Canvas(root, bd=0, height=h, width=w, highlightthickness=0, bg='black')
