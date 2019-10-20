437f5b85868b96c2594bb6a67597fe7dc99e62f9
nestark <nestark@users.noreply.github.com>
2019-10-20 11:01:43 +0800

    added Wuhan location. remove toggle_fullscreen method.

--- screen5.py
+++ screen5.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from screen3 import FullScreenApp
+
+
 class CanvasButton:
     def __init__(self, canvas):
         self.canvas = canvas
@@ -7,38 +9,43 @@ class CanvasButton:
         self.createWidgets(canvas)
 
     def createWidgets(self, canvas):
-        locations=('雍和/惠通','达美','成都','上海','大连','深圳')
-        self.shorts=('yh','dm','cd','sh','dl','sz')
+        locations = ('雍和/惠通', '达美', '成都', '上海', '大连', '深圳','武汉')
+        self.shorts = ('yh', 'dm', 'cd', 'sh', 'dl', 'sz', 'wh')
         self.intVar = IntVar()
         i = 1
         for location in locations:
-            if i<=3:
-                Radiobutton(canvas, text=location,variable = self.intVar,# 将Radiobutton绑定到self.intVar变量
-                    value=i,font = ('SimHei', 13),bg='grey',fg = 'black',highlightcolor='black',
-                            ).place(x=w/2-310+135*i,y=h/2-95)
-            else:
+            if i <= 3:
+                Radiobutton(canvas, text=location, variable=self.intVar,  # 将Radiobutton绑定到self.intVar变量
+                            value=i, font=('SimHei', 13), bg='grey', fg='black', highlightcolor='black',
+                            ).place(x=w / 2 - 310 + 135 * i, y=h / 2 - 95)
+                i += 1
+            elif i == 7:
                 Radiobutton(canvas, text=location, variable=self.intVar,
-                            value=i,font = ('SimHei', 13),bg='grey',fg = 'black',highlightcolor='black'
-                            ).place(x=w/2-310+135*(i-3), y=h / 2 - 65)
-            i+=1
+                            value=i, font=('SimHei', 13), bg='grey', fg='black', highlightcolor='black'
+                            ).place(x=w / 2 - 310 + 135 * (i - 6), y=h / 2 - 35)
+            elif 3 < i <= 6:
+                Radiobutton(canvas, text=location, variable=self.intVar,
+                            value=i, font=('SimHei', 13), bg='grey', fg='black', highlightcolor='black'
+                            ).place(x=w / 2 - 310 + 135 * (i - 3), y=h / 2 - 65)
+                i += 1
         self.intVar.set(1)
         button = Button(canvas, image=img1, bd=0, command=self.buttonclicked)
-        self.id = canvas.create_window(w / 2, h / 2 -15, window=button)  # create button and put it on canvas window
+        canvas.create_window(w / 2, h / 2 + 40, window=button)  # create button and put it on canvas window
 
-    def buttonclicked(self,*args):
+    def buttonclicked(self, *args):
         num = self.intVar.get()
-        names = self.shorts[num-1]
-        with open(Epath,'w') as file_object:  # write username to namepath
+        names = self.shorts[num - 1]
+        with open(Epath, 'w') as file_object:  # write username to namepath
             file_object.write(names)
         root.destroy()  # end the window
+
 
 if __name__ == "__main__":
     root = Tk()
     app = FullScreenApp(root)
-    app.toggle_fullscreen()
     w = root.winfo_screenwidth()
     h = root.winfo_screenheight()
-    Epath=r'c:\ittools\zz.txt'
+    Epath = r'c:\ittools\zz.txt'
     img = PhotoImage(file='zhichang.png')
     img1 = PhotoImage(file='Enter.png')
     canvas = Canvas(root, bd=0, height=h, width=w, highlightthickness=0, bg='black')
